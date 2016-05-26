7873f2f059e906c1ca899bf7c247bbecd1d014f5
miguel76 <miguel.ceriani@gmail.com>
2016-05-26 00:44:01 -0300

    Make depth-first work

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-module.exports = require('./lib/graph');
+var makeGraph = require('./lib/graph'),
+    makeVisit = require('./lib/visit'),
+    commonVisits = require('./lib/commonVisits');
+
+module.exports = {
+  makeGraph: makeGraph,
+  makeVisit: makeVisit,
+  commonVisits: commonVisits
+};
--- test/test.js
+++ test/test.js
@@ -4,9 +4,45 @@ var fs = require('fs'),
 var inputStr = fs.readFileSync(__dirname + '/library.jsonld');
 var inputJson = JSON.parse(inputStr);
 
-module.exports = function(obj) {
-  graph(inputJson, null, function(err, result) {
-    if (err) obj.err = err;
-    if (result) obj.result = result;
-  });
+var show = function(node) {
+  if (node['@id']) return { '@id': node['@id'] };
+  if (node['@value']) return { '@value': node['@value'] };
+  return node;
 };
+
+var depth = 0;
+
+var depthFirst = graph.commonVisits.depthFirst({
+  preAction: function(node, parentInVisit) {
+    depth++;
+    if (depth > 5) throw 'Too much';
+    console.log('Going down!');
+    console.log('- Now in node:');
+    console.log(show(node));
+    console.log('- Coming from:');
+    console.log(parentInVisit ? {
+      node: show(parentInVisit.node),
+      leadingProperty: parentInVisit.leadingProperty,
+      leadingPropertyIsReverse: parentInVisit.leadingPropertyIsReverse
+    } : undefined);
+    console.log('- Depth ' + depth);
+  },
+  postAction: function(node, children, preActionValue) {
+    depth--;
+    console.log('Going up!');
+    console.log('- Now in node:');
+    console.log(show(node));
+    console.log('- Coming from:');
+    console.log(children);
+    console.log('Depth ' + depth);
+  }
+});
+
+
+graph.makeGraph(inputJson, null, function(err, result) {
+  if (err) {
+    console.log(err);
+  } else if (result) {
+    depthFirst(result['http://example.org/library/the-republic']);
+  }
+});
